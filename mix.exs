58e125e36c4dab3908fc633a4aca02f0ab80445a
Ivan Sh <ivan@forzafc.football>
2019-10-08 12:13:44 +0300

    updated version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EctoMaterializedPath.Mixfile do
   use Mix.Project
 
-  @project_url "https://github.com/asiniy/ecto_materialized_path"
+  @project_url "https://github.com/IvanShpotenko/ecto_materialized_path"
   @version "0.2.1"
 
   def project do
